9ce5f3a6ac25a032a8903e131ff8f118324f92cb
Bernardo <bernardo.martins@newgo.com.br>
2023-02-26 19:53:42 -0300

    fix: create-device.schema
    -- Remove full request schema
    -- export expected body schema

--- src/devices/schemas/create-device.schema.ts
+++ src/devices/schemas/create-device.schema.ts
@@ -1,13 +1,9 @@
 import z from 'zod'
 
-const createDeviceSchema = z.object({
+export const createDeviceSchema = z.object({
   name: z.string(),
   description: z.string(),
   topic: z.string()
 })
 
 export type CreateDeviceType = z.infer<typeof createDeviceSchema>
-
-export const CreateDeviceRequestSchema = z.object({
-  body: createDeviceSchema
-})
