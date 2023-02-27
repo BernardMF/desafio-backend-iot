affd844f921835c8c5ef234427780cb2ded2ef6b
Bernardo <bernardo.martins@newgo.com.br>
2023-02-26 23:22:20 -0300

    feat!: Device Service CRUD Functions
    -- Create, Remove, Update, Delete methods

--- src/devices/devices.service.ts
+++ src/devices/devices.service.ts
@@ -1,8 +1,12 @@
-import { createDeviceSchema, CreateDeviceType } from './schemas/create-device.schema'
+import { createDeviceSchema, CreateDeviceType, deviceSchema, DeviceType } from './schemas/create-device.schema'
 import { validateRequestSchema } from '../utils'
 import { DeviceModel } from '../repositories/database/models/deviceModel'
+import { BadRequest } from 'http-errors'
+import { Types } from 'mongoose'
+
 const schemas = {
-  create: createDeviceSchema
+  create: createDeviceSchema,
+  query: deviceSchema
 }
 
 export function validateBody(body: unknown, caller: string): CreateDeviceType {
@@ -13,4 +17,26 @@ export async function saveDevice(body: CreateDeviceType): Promise<any> {
   // Insert device in database
   const device = new DeviceModel(body)
   return device.save()
+}
+
+export async function findDeviceById(id: Types.ObjectId): Promise<any> {
+  const device = await DeviceModel.findById(id)
+  console.debug('device found', device)
+  if (!device) {
+    throw new BadRequest('Device not found')
+  }
+  return device
+}
+
+export async function queryDevices(query: Partial<DeviceType>) {
+  return DeviceModel.find(query)
+}
+
+export async function deleteDevice(id: string) {
+  return DeviceModel.findByIdAndDelete(id)
+}
+
+export async function updateDevice(id: Types.ObjectId, body: Partial<CreateDeviceType>) {
+  const device = await findDeviceById(id)
+  return device.update(body)
 }
