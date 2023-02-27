bdaac88749255fa6e90519c759ee572ab4c78f35
Bernardo <bernardo.martins@newgo.com.br>
2023-02-26 22:44:26 -0300

    feat: Add full zod schema
    -- To validate queries used by get /devices route

--- src/devices/schemas/create-device.schema.ts
+++ src/devices/schemas/create-device.schema.ts
@@ -7,3 +7,15 @@ export const createDeviceSchema = z.object({
 })
 
 export type CreateDeviceType = z.infer<typeof createDeviceSchema>
+
+const timestampRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/
+export const deviceSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  topic: z.string(),
+  createdAt: z.string().regex(timestampRegex),
+  updatedAt: z.string().regex(timestampRegex)
+})
+
+export type DeviceType = z.infer<typeof deviceSchema>
