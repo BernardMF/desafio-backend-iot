5c39d93286abd38ca08bad712a961490c108964d
Bernardo <bernardo.martins@newgo.com.br>
2023-02-27 16:54:57 -0300

    feat!: Set device routes

--- src/devices/devices.routes.ts
+++ src/devices/devices.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
-import { createDevice, getDevice, getDeviceById } from './devices.controller'
+import { createDevice, queryDevices, getDeviceById, updateDevice, deleteDevice } from './devices.controller'
 const router = Router()
 
 router.post('/', createDevice)
 
-router.get('/', getDevice)
+router.get('/', queryDevices)
 
 router.get('/:id', getDeviceById)
+
+router.post('/:id', updateDevice)
+
+router.delete('/:id', deleteDevice)
 
 export const devicesRouter = router
